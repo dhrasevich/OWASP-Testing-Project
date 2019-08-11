397aef00139c9bee09c97cd8c72dd30dee98b97f
y.kazheka <y.kazheka@godeltech.com>
2019-08-11 22:43:34 +0300

    add scripts for creation of album, artist and genre tables; add script with seed data

---
+++ GodelTech.Owasp.Database/Tables/Album.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Album]
+(
+	[AlbumId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [GenreId] INT NULL FOREIGN KEY REFERENCES Genre(GenreId), 
+    [ArtistId] INT NULL FOREIGN KEY REFERENCES Artist(ArtistId), 
+    [Title] NVARCHAR(160) NULL, 
+    [Price] DECIMAL(18, 2) NULL, 
+    [AlbumArtUrl] NVARCHAR(1024) NULL
+)
